0a32e48664b9d6fd57b9e36a61ad46a9bbde2d3c
Tiffany Bennett <tiffany@stormbit.net>
2014-12-29 13:30:48 -0500

    Add an #error when no function loader is selected

--- src/tgl/gl.h
+++ src/tgl/gl.h
@@ -7,6 +7,9 @@
 #elif defined(TGL_USE_EPOXY)
 #include <epoxy/gl.h>
 #define TGL_EXTENSION(n) epoxy_has_gl_extension("GL_" #n)
+#else
+#error You must provide a GL function loader. Currently, GLEW and libepoxy are supported. Use \
+    -DTGL_USE_GLEW or -DTGL_USE_EPOXY
 #endif
 
 #endif
